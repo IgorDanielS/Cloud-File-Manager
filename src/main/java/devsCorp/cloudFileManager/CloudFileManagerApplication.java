ab39ee9fbcfa580ece343c0b38362ab428a41298
IgorDanielS <igordaniel1903@gmail.com>
2025-01-09 13:25:42 -0300

    project initialized and cloud database connected

---
+++ src/main/java/devsCorp/cloudFileManager/CloudFileManagerApplication.java
@@ -0,0 +1,13 @@
+package devsCorp.cloudFileManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFileManagerApplication.class, args);
+	}
+
+}
